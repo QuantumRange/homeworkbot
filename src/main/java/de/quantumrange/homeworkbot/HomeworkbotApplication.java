e166a171c105962bed97712aa3988974b88e218d
QuantumRange <justusst@mailbox.org>
2022-02-02 08:47:19 +0100

    Initial commit

---
+++ src/main/java/de/quantumrange/homeworkbot/HomeworkbotApplication.java
@@ -0,0 +1,13 @@
+package de.quantumrange.homeworkbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/quantumrange/homeworkbot/HomeworkbotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.quantumrange.homeworkbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
